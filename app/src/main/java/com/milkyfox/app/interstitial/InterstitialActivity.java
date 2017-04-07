489aa2dcc40994db01862275037fe5fa7a42bfde
AndroidMilkyFox <android@milkyfox.com>
2017-04-07 12:12:22 +0300

    some for styles

--- app/src/main/java/com/milkyfox/app/interstitial/InterstitialActivity.java
+++ app/src/main/java/com/milkyfox/app/interstitial/InterstitialActivity.java
@@ -10,16 +10,20 @@ package com.milkyfox.app.interstitial;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.milkyfox.app.R;
 
 public class InterstitialActivity extends AppCompatActivity {
+    Toolbar mToolBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interstitial);
 
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolBar);
     }
 }
 
